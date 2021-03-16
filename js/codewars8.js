7702fd30f906c6cebb7ecfcdb39b4ae6ee92bbd5
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-03-15 20:26:17 -0400

    completed a 5kyu challenge

--- js/codewars8.js
+++ js/codewars8.js
@@ -1,3 +1,5 @@
+//T.T.T.17: Split odd and even
+//https://www.codewars.com/kata/57a2ab1abb994466910003af/train/javascript
 function splitOddAndEven(num) {
   let result = "";
   let arr = num.toString().split("");
@@ -12,3 +14,22 @@ function splitOddAndEven(num) {
 }
 
 splitOddAndEven(12839393643493);
+
+//Human readable time
+//https://www.codewars.com/kata/52685f7382004e774f0001f7/train/javascript
+function humanReadable(seconds) {
+  let hour = Math.floor(seconds / 3600);
+  let minute = Math.floor((seconds % 3600) / 60);
+  let second = Math.floor(((seconds % 3600) % 60) % 60);
+  let result = [hour, minute, second];
+  result = result
+    .map((char) => {
+      if (char.toString().length == 1) return "0" + char.toString();
+      else return char.toString();
+    })
+    .join(":");
+  return result;
+}
+
+humanReadable(359999);
+humanReadable(60);
