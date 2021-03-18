a7e4b82d3d8f28ad416017e6444952773e15e0cb
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-03-17 21:06:35 -0400

    added URL of kata

--- js/codewars9.js
+++ js/codewars9.js
@@ -1,19 +1,5 @@
-//Utility Function
-function humanReadable(seconds) {
-  let hour = Math.floor(seconds / 3600);
-  let minute = Math.floor((seconds % 3600) / 60);
-  let second = Math.floor(((seconds % 3600) % 60) % 60);
-  let result = [hour, minute, second];
-
-  result = result
-    .map((char) => {
-      if (char.toString().length == 1) return "0" + char.toString();
-      else return char.toString();
-    })
-    .join("|");
-  return result;
-}
-
+//6kyu Took longer than expected
+//https://www.codewars.com/kata/55b3425df71c1201a800009c
 function stat(strg) {
   if (strg === "") return strg;
   let resultInSeconds = strg
@@ -44,6 +30,22 @@ function stat(strg) {
   return `Range: ${humanReadable(range)} Average: ${humanReadable(
     average
   )} Median: ${humanReadable(median)}`;
+}
+
+//Utility Function
+function humanReadable(seconds) {
+  let hour = Math.floor(seconds / 3600);
+  let minute = Math.floor((seconds % 3600) / 60);
+  let second = Math.floor(((seconds % 3600) % 60) % 60);
+  let result = [hour, minute, second];
+
+  result = result
+    .map((char) => {
+      if (char.toString().length == 1) return "0" + char.toString();
+      else return char.toString();
+    })
+    .join("|");
+  return result;
 }
 
 console.log(stat("01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"));
