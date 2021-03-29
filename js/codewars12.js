2c3c17f8a287bef4c562c5fac90aeb511c2a1405
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-03-28 22:34:03 -0400

    completed a 6kyu challenge

--- js/codewars12.js
+++ js/codewars12.js
@@ -1,1 +1,46 @@
 //https://www.codewars.com/kata/587a88a208236efe8500008b
+
+function findSenior(list) {
+  let max = Math.max.apply(
+    null,
+    list.map((person) => person.age)
+  );
+  return list.filter((char) => char.age == max);
+}
+
+var list1 = [
+  {
+    firstName: "Gabriel",
+    lastName: "X.",
+    country: "Monaco",
+    continent: "Europe",
+    age: 49,
+    language: "PHP",
+  },
+  {
+    firstName: "Odval",
+    lastName: "F.",
+    country: "Mongolia",
+    continent: "Asia",
+    age: 38,
+    language: "Python",
+  },
+  {
+    firstName: "Emilija",
+    lastName: "S.",
+    country: "Lithuania",
+    continent: "Europe",
+    age: 19,
+    language: "Python",
+  },
+  {
+    firstName: "Sou",
+    lastName: "B.",
+    country: "Japan",
+    continent: "Asia",
+    age: 49,
+    language: "PHP",
+  },
+];
+
+console.log(findSenior(list1));
