8133580b9878f81e13f9a4af9022373975fa746d
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-04-28 20:30:14 -0400

    cleaned solution

--- js/codewars13.js
+++ js/codewars13.js
@@ -3,18 +3,10 @@ const kataLink = "codewars.com/kata/578aa45ee9fd15ff4600090d/train/javascript";
 //Sort the Odd numbers but leave the even in position.
 
 function sortArray(arr) {
-  let oddArrSorted = arr.filter((num) => num % 2 != 0).sort((a, b) => a - b);
-  let sortedArr = [];
-
-  let start = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0) sortedArr.push(arr[i]);
-    else sortedArr.push(oddArrSorted[start++]);
-  }
-
-  return sortedArr;
+  let oddArr = arr.filter((num) => num % 2 != 0).sort((a, b) => a - b);
+  return arr.map((num) => (num % 2 == 0 ? num : oddArr.shift()));
 }
 
 let test1 = [5, 8, 6, 3, 4];
 
-SortArray(test1);
+console.log(sortArray(test1));
