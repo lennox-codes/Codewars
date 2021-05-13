8012fa546177ef41c44401d9d163bb283e17f626
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-12 20:54:51 -0400

    solved another challenge

--- js/codewars14.js
+++ js/codewars14.js
@@ -31,12 +31,36 @@ function longest(str) {
   }
 }
 
-// One:
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
-
 function multiply(arr) {
   return arr.reduce((a, c) => a * c, 1);
 }
 
 console.log(multiply([1, 2, 3, 4]));
+
+// Two:
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+function ageStats(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  return [min, max, max - min];
+}
+
+console.log(ageStats([1, 2, 3, 5]));
+
+// Three:
+// Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+// Example:
+// [ 6, 2, 1, 8, 10 ] => 16
+// [ 1, 1, 11, 2, 3 ] => 6
+function sumExceptMinAndMax(arr) {
+  return arr
+    .sort((a, b) => a - b)
+    .slice(1, arr.length - 1)
+    .reduce((a, b) => a + b, 0);
+}
+
+console.log(ageStats([1, 2, 3, 5]));
+
+console.log(sumExceptMinAndMax([6, 2, 1, 8, 10]));
