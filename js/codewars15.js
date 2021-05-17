9879fda1e3a8860503eb75bbf828b2307ce6f447
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-05-17 14:21:45 -0400

    saved file

--- js/codewars15.js
+++ js/codewars15.js
@@ -1,7 +1,8 @@
-function chuckPushUps(string) {
-  let count = string.match(/1|0/g);
-  console.log(count);
-  return typeof string !== "string" ? "FAIL!!" : count.length ? parseInt(count.join("")) : "CHUCK SMASH!!";
-}
+//https://www.codewars.com/kata/570564e838428f2eca001d73
+// function chuckPushUps(string) {
+//   let count = string.match(/1|0/g);
+//   console.log(count);
+//   return typeof string !== "string" ? "FAIL!!" : count.length ? parseInt(count.join("")) : "CHUCK SMASH!!";
+// }
 
-chuckPushUps("hwo are we doin today");
+// console.lchuckPushUps("hwo are we doin today");
